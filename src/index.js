4b2403b2bd3082084049189575003539faf05c7f
Paul Caruana <paul@beambient.co.uk>
2020-08-12 18:43:30 +0100

    Cleaner syntax using setState

--- src/index.js
+++ src/index.js
@@ -17,20 +17,37 @@ const Book = ({title, author, pages}) => {
     </section>
   )
 }
-const Library = ({books}) => {
-  return (
-    <div>
-      {books.map(
-        (book, i) => 
-          <Book
-            key={i}
-            title={book.title} 
-            author={book.author} 
-            pages={book.pages} />
-      )}
-    </div>
-  )
+class Library extends React.Component {
+ 
+  state = {open: false}
+  
+  toggleOpenClosed= () => {
+    this.setState(prevState => ({
+      open: !prevState.open
+    }))
+  }
+  render() {
+    const {books} = this.props
+    return (
+      <div>
+        <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
+        <button onClick={this.toggleOpenClosed}>Change</button>
+        {books.map(
+          (book, i) => 
+            <Book
+              key={i}
+              title={book.title} 
+              author={book.author} 
+              pages={book.pages} />
+        )}
+
+      </div>
+    )
+
+  }
 }
+
+
 
 render(
   <Library books={bookList}/>, 
